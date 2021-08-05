1e7dfb1762ffc1febff03c8d534924abd13f1c43
Seulgi_LEE <dl4459@naver.com>
2021-08-05 17:04:55 +0900

    8/5 Sort algorism

--- 04 Sort/Q25 Failure Rate.py
+++ 04 Sort/Q25 Failure Rate.py
@@ -32,3 +32,23 @@ N = 4
 stages = [4,4,4,4,4]
 
 print(solution(N, stages))
+
+
+###################################   답안 예시    ###################################
+
+def solution(N, stages):
+  answer = []
+  length = len(stages)
+  for i in range(1, N+1):
+    # 해당 스테이지에 머물러 있는 사람 수 계산
+    count = stages.count(i)
+    # 실패율 계산
+    if length == 0:
+      fail = 0
+    else:
+      fail = count / length
+    answer.append((i, fail))
+    length -= count # 스테이지에 머물러 있는 사람을 뺀다( 그보다 아래에 있는 사람들가지고 계산 )
+  answer = sorted(answer, key=lambda t: t[1], reverse=True) # 실패율 기준으로 내림차순으로 정렬
+  answer = [i[0] for i in answer]
+  return answer
--- 04 Sort/Q26. Sort Card.py
+++ 04 Sort/Q26. Sort Card.py
@@ -25,3 +25,29 @@ for card in nPr:
   answer.append(result)
 
 print(min(answer))
+
+
+###################################   답안 예시    ###################################
+
+import heapq
+
+n = int(input())
+
+# 힙(Heap)에 초기 카드 묶음을 모두 삽입한다.
+heap = []
+for i in range(n):
+  data = int(input())
+  heapq.heappush(heap, data)
+
+result = 0
+while len(heap) != 1:
+  # 가장 작은 2개의 카드 묶음을 꺼내기
+  one = heapq.heappop(heap)
+  two = heapq.heappop(heap)
+  # 카드 묶음을 합쳐서 다시 삽입
+  sum_value = one + two
+  # 합친 카드를 result에 더함
+  result += sum_value
+  heapq.heappush(heap, sum_value)
+
+print(result)
