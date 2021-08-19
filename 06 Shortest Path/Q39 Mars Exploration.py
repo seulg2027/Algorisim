e40a35162b1563c2ee3e7bba45374408517f1bbb
Lee-Developer-git <seulgi2027@dgu.ac.kr>
2021-08-20 00:34:06 +0900

    8/19 Shortest Path

--- 06 Shortest Path/Q39 Mars Exploration.py
+++ 06 Shortest Path/Q39 Mars Exploration.py
@@ -22,6 +22,7 @@ for _ in range(int(input())):
         graph[i][j-1].append((i, j, data[i][j]))
 
   distance = [[INF] * N for _ in range(N)]
+  print(graph)
 
   # 0,0 부터 N-1, N-1까지 중 최단 경로
   def dijkstra():
@@ -34,9 +35,50 @@ for _ in range(int(input())):
         continue
       for i in graph[x][y]:
         next_dist = distance[x][y] + i[2]
-        if next_dist < distance[i[0]][i[1]]:
+        if next_dist < distance[i[0]][i[1]]: # 아직 실행 안했을 때 
           distance[i[0]][i[1]] = next_dist
-          heapq.heappush(q, (i[0], i[1], next_dist))
+          heapq.heappush(q, (i[0], i[1], distance[i[0]][i[1]]))
 
   dijkstra()
-  print(distance)
+  print(distance[N-1][N-1])
+
+
+########################## 답안 예시 ##########################
+
+
+import sys
+input = sys.stdin.readline
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+for tc in range(int(input())):
+  n = int(input())
+  graph = []
+  for i in range(n):
+    graph.append(list(map(int, input().split())))
+  
+  distance = [[INF] * n for _ in range(n)]
+  #### 여기까진 같음
+  
+  x, y = 0, 0
+  q =[(graph[x][y], x, y)]
+  distance[x][y] = graph[x][y]
+  while q:
+    dist, x, y = heapq.heappop(q)
+    if distance[x][y] < dist:
+      continue
+    # 아 이거 bfs에서 맨날 봤던,,, 그런 형태인데
+    # 근데 왜 4번이나 돌리지..? 두번만 돌려도 되지 않나
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+      if nx < 0 or nx >= n or ny < 0 or ny >= n:
+        continue
+      # 현재 노드 비용 + 그다음 노드로 이동하는 비용
+      cost = dist + graph[nx][ny]
+      if cost < distance[nx][ny]:
+        distance[nx][ny] = cost
+        heapq.heappush(q, (cost, nx, ny))
+  
+  print(distance[n-1][n-1])
