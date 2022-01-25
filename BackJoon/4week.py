820b6c7ec563282294fe1e1a4232f4d28580f726
Seulg2027 <seulgi2027@dgu.ac.kr>
2022-01-25 16:33:19 +0900

    멋쟁이스터디 4주차 이진탐색2

--- BackJoon/4week.py
+++ BackJoon/4week.py
@@ -1,31 +1,53 @@
-# 기타 레슨
+# # 기타 레슨
 
-import sys
-input = sys.stdin.readline
+# import sys
+# input = sys.stdin.readline
 
-n, m = map(int, input().split())
-lessons = list(map(int, input().split()))
+# n, m = map(int, input().split())
+# lessons = list(map(int, input().split()))
 
-minlesson = max(lessons)
-sumlesson = sum(lessons)
-ans = int(1e9)
+# minlesson = max(lessons)
+# sumlesson = sum(lessons)
+# ans = int(1e9)
 
-while minlesson <= sumlesson:
-  mid = (minlesson + sumlesson) // 2
-  cnt = 0
-  sum_num = 0
-  for i in range(len(lessons)):
-    if sum_num + lessons[i] > mid:
-      cnt += 1
-      sum_num = 0
-    sum_num += lessons[i]
-  if sum_num:
-    cnt += 1
+# while minlesson <= sumlesson:
+#   mid = (minlesson + sumlesson) // 2
+#   cnt = 0
+#   sum_num = 0
+#   for i in range(len(lessons)):
+#     if sum_num + lessons[i] > mid:
+#       cnt += 1
+#       sum_num = 0
+#     sum_num += lessons[i]
+#   if sum_num:
+#     cnt += 1
   
-  if cnt > m:
-    minlesson = mid + 1
-  else:
-    ans = min(ans, mid)
-    sumlesson = mid - 1
+#   if cnt > m:
+#     minlesson = mid + 1
+#   else:
+#     ans = min(ans, mid)
+#     sumlesson = mid - 1
 
-print(ans)
+# print(ans)
+
+# 게임 
+
+X, Y = map(int, input().split())
+Z = (Y * 100) // X
+
+if Z >= 99:
+  print(-1)
+else:
+  answer = 0
+  left = 1
+  right = X
+  
+  while left <= right:
+    mid = (left + right) // 2
+    if ((Y + mid) * 100 // (X + mid)) <= Z:
+      left = mid+1
+    else:
+      answer = mid
+      right = mid - 1
+  
+  print(answer)
