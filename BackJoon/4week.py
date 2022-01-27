d7241fe31e551276b4bf137df168074253f12447
Seulg2027 <seulgi2027@dgu.ac.kr>
2022-01-27 22:18:07 +0900

    멋쟁이스터디 4주차 이진탐색3

--- BackJoon/4week.py
+++ BackJoon/4week.py
@@ -30,24 +30,70 @@
 
 # print(ans)
 
-# 게임 
+# # 게임 
 
-X, Y = map(int, input().split())
-Z = (Y * 100) // X
+# X, Y = map(int, input().split())
+# Z = (Y * 100) // X
 
-if Z >= 99:
-  print(-1)
-else:
-  answer = 0
-  left = 1
-  right = X
+# if Z >= 99:
+#   print(-1)
+# else:
+#   answer = 0
+#   left = 1
+#   right = X
   
-  while left <= right:
-    mid = (left + right) // 2
-    if ((Y + mid) * 100 // (X + mid)) <= Z:
-      left = mid+1
+#   while left <= right:
+#     mid = (left + right) // 2
+#     if ((Y + mid) * 100 // (X + mid)) <= Z:
+#       left = mid+1
+#     else:
+#       answer = mid
+#       right = mid - 1
+  
+#   print(answer)
+
+# # 숫자 카드 2
+
+# import sys
+# from bisect import bisect_right, bisect_left
+# input = sys.stdin.readline
+
+# n = int(input())
+# cards = list(map(int, input().split()))
+# m = int(input())
+# data = list(map(int, input().split()))
+
+# cards.sort()
+
+# def binary_search(arr, left_value, right_value):
+#   right_index = bisect_right(arr, right_value)
+#   left_index = bisect_left(arr, left_value)
+#   return right_index - left_index
+
+# result_list = []
+
+# for i in range(m):
+#   result = binary_search(cards, data[i], data[i])
+#   result_list.append(result)
+
+# for j in range(m):
+#   print(result_list[j], end=' ')
+
+# 제곱근
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+def binary_search(target, start, end):
+  while start <= end:
+    mid = (start + end) // 2
+    if mid ** 2 == target:
+      return mid
+    elif mid ** 2 > target:
+      end = mid - 1
     else:
-      answer = mid
-      right = mid - 1
-  
-  print(answer)
+      start = mid + 1
+
+print(binary_search(n, 0, n // 2 - 1))
